ac93bfeeec1f2f6adf8925e4a2b7ee7fe69605cb
Will Eccles <willeccles860+github@gmail.com>
2016-09-14 19:01:17 -0400

    Removed --progress from git pull

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 echo "Backing up settings.json..."
 mv settings.json settings.json.backup
 echo "Updating server..."
-git pull --progress
+git pull
 echo "Replacing settings.json..."
 mv settings.json.backup settings.json
