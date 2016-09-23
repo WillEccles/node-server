767dffb1586c476304c2046a9b203c45782e5178
Will Eccles <willeccles860+github@gmail.com>
2016-09-23 15:28:58 -0400

    Updated update script

--- update.sh
+++ update.sh
@@ -2,8 +2,15 @@
 
 echo "Backing up settings.json..."
 mv settings.json settings.json.backup
+echo "Backing up users.json..."
+mv users.json users.json.backup
+echo "Backing up index.html..."
+mv index.html index.html.backup
 echo "Updating server..."
 git pull
 echo "Replacing settings.json..."
 mv settings.json.backup settings.json
-rm settings.json.backup
+echo "Replacing users.json..."
+mv users.json.backup users.json
+echo "Replacing index.html..."
+mv index.html.backup index.html
